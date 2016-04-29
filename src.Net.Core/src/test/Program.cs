55d3fd748da87806add99361c7d366745ed52013
pelhu <khamzat@live.com>
2016-04-29 18:48:40 +0300

    copyright

--- src.Net.Core/src/test/Program.cs
+++ src.Net.Core/src/test/Program.cs
@@ -1,8 +1,10 @@
-﻿using Renci.SshNet;
+﻿using Flurl;
+using Renci.SshNet;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace test
@@ -27,7 +29,8 @@ namespace test
             //}
 
 
-            PasswordConnectionInfo conn = new PasswordConnectionInfo("[IP]", 22, "root", "[password]");
+            PasswordConnectionInfo conn = new PasswordConnectionInfo("172.17.7.191", 22, "clog", "ctech99.99clog");
+            //PasswordConnectionInfo conn = new PasswordConnectionInfo("172.17.7.88", 22, "root", "ctech99.99clog");
             SftpClient sftpClient = new SftpClient(conn);
             sftpClient.Connect();
 
@@ -38,8 +41,71 @@ namespace test
             //    input.CopyToAsync(output).Wait();
             //}
 
-            var d = sftpClient.ListDirectory("/");
-            Console.WriteLine(string.Join(", ", d.Select(s => s.Name)));
+
+            sftpClient.EnsureDirectoryExist("cryptolog/workers/parser");
+            
+            //var d = sftpClient.ListDirectory("");
+            //Console.WriteLine(string.Join(", ", d.Select(s => s.Name)));
+
+            //sftpClient.UploadFile(File.OpenRead(@"C:\Users\khamzatsalikhov\Source\Workspaces\MicroCollection\DistributedCryptolog\Main\src\DistributedCryptolog\workers\parser\azure.JPG"), "/home/clog/cryptolog/workers/parser/azure.JPG");
+
+        }
+    }
+
+    public static class Extentions
+    {
+        public static void EnsureDirectoryExist(this SftpClient sftpClient, string directory)
+        {
+            if (directory!= null && directory.Contains('\\'))
+            {
+                directory = directory.Replace('\\', '/'); 
+            }
+
+            if (string.IsNullOrWhiteSpace(directory) || !directory.Any(l => l != '/')) return;
+
+            if (!sftpClient.Exists(directory))
+            {
+                //check if parent exists
+                    var parent = Regex.Replace(directory, @"[^/].?[/$]", "");
+                sftpClient.EnsureDirectoryExist(parent);
+
+                sftpClient.CreateDirectory(directory);
+            }
+        }
+
+        public static void UploadDirectory(this SftpClient sftpClient, string sourceDir, string destDir, bool overWrite = false)
+        {
+            if (string.IsNullOrWhiteSpace(sourceDir))
+            {
+                throw new ArgumentException("Path must not be empty", nameof(sourceDir));
+            }
+
+            if (string.IsNullOrWhiteSpace(destDir))
+            {
+                throw new ArgumentException("Path must not be empty", nameof(destDir));
+            }
+
+            if (!Directory.Exists(sourceDir))
+            {
+                throw new DirectoryNotFoundException($"Directory \"{sourceDir}\" not exists");
+            }
+
+            foreach (var file in Directory.GetFiles(sourceDir))
+            {
+                try
+                {
+                    sftpClient.UploadFile(File.OpenRead(file), destDir.AppendPathSegment(Path.GetFileName(file)));
+                }
+                catch (Exception ex)
+                {
+                    throw new IOException($"Error uploading directory {sourceDir} to {destDir} on file {Path.GetFileName(file)}", ex);
+                }
+            }
+
+            foreach (var dir in Directory.GetDirectories(sourceDir))
+            {
+                sftpClient.UploadDirectory(dir, destDir.AppendPathSegment(Path.GetDirectoryName(dir)));
+            }
         }
     }
 }
